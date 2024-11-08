393a52e74b5b3116bbc85b137976120c1084c993
raveenar0505 <163967728+raveenar0505@users.noreply.github.com>
2024-11-08 14:27:55 +0530

    Add files via upload

---
+++ task 2/script.js
@@ -0,0 +1,8 @@
+// JavaScript to handle interactions, such as showing more courses
+document.addEventListener("DOMContentLoaded", () => {
+    const exploreBtn = document.querySelector(".explore-btn");
+    exploreBtn.addEventListener("click", () => {
+      window.location.href = "#categories";
+    });
+  });
+  
